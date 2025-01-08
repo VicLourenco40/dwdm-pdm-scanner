6114f7c37b5a72af5724e686b100dddbc159a258
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-01-08 19:03:31 +0000

    Revert "Make scanned ULIDs uppercase"
    This reverts commit 83ae2adc9290125063abd4558a15f58462fcb16d.

--- app/src/main/java/com/viclourenco/ticketscanner/MainScreen.kt
+++ app/src/main/java/com/viclourenco/ticketscanner/MainScreen.kt
@@ -165,7 +165,7 @@ fun MainScreen(
                         .addOnSuccessListener { barcode ->
                             val ticketCode = barcode.rawValue
                             if (ticketCode != null) {
-                                mainViewModel.getTicketPurchase(ticketCode.uppercase())
+                                mainViewModel.getTicketPurchase(ticketCode.lowercase())
                             }
                         }
                 },
